a39fc9437c67dcdd3961ac372fc90f6f45ad2f41
FouziaBibi00 <168702663+FouziaBibi00@users.noreply.github.com>
2024-05-08 10:56:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+var menuBtn = document.getElementById("menuBtn");
+var sideNav = document.getElementById("sideNav");
+var menu = document.getElementById("menu");
+
+sideNav.style.right = "-250px";
+
+menuBtn.onclick = function () {
+    if (sideNav.style.right == "-250px") {
+        sideNav.style.right = "0";
+        menu.src = "img/close.png";
+    }
+    else {
+        sideNav.style.right = "-250px";
+        menu.src = "img/menu.png";
+    }
+}
+
+
+
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("item");
+  if (c === "all") {
+      for (i = 0; i < x.length; i++) {
+          x[i].style.display = "block";
+      }
+  } else {
+      for (i = 0; i < x.length; i++) {
+          if (x[i].classList.contains(c)) {
+              x[i].style.display = "block";
+          } else {
+              x[i].style.display = "none";
+          }
+      }
+  }
+}
